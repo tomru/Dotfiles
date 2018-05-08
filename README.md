f9c702ed55141d04661c2428ae3212bdd353164d
Thomas Ruoff <ThomasRuoff@gmail.com>
2018-05-09 00:08:38 +0200

    use GNU stow for setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# My dotfiles
+
+## Requirements
+
+* [GNU stow](https://www.gnu.org/software/stow/).
+
+## Install
+
+### All applications
+
+* Run `./setup.sh`
+
+### One application
+
+* Run `stow <application>`
+
+
--- setup.sh
+++ setup.sh
@@ -1,28 +1,8 @@
 INSTALL_DIR=~/.dotfiles
 TARGET_DIR=$HOME
 
-for ITEM in `ls $INSTALL_DIR` ; do
+cd $INSTALL_DIR
 
-	if [ "$ITEM" = "setup.sh" ] 
-	then
-		continue; 
-	fi
-
-	TARGET_NAME="$TARGET_DIR/.$ITEM";
-	
-	# backup needed
-	if [ -f $TARGET_NAME ] || [ -h $TARGET_NAME ] || [ -d $TARGET_NAME ]
-	then
-		echo "Found $TARGET_NAME. Backing up to ${TARGET_NAME}.pre_dotfiles";
-		mv $TARGET_NAME ${TARGET_NAME}.pre_dotfiles || exit 1
-	fi
-
-	ln -s $INSTALL_DIR/$ITEM $TARGET_NAME;
-	echo "Genereated symbolic link from $TARGET_NAME to $INSTALL_DIR/$ITEM";
-	echo ""
-
-
-done
-
+stow $(ls -d */ | xargs basename --multiple)
 echo 'dotfiles are installed'
 
