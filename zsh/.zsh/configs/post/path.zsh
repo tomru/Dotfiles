0eab4e99dc27a412077ebd9b2e21f251d7260ddb
Thomas Ruoff <ThomasRuoff@gmail.com>
2018-06-14 09:03:56 +0200

    remove rbenv and nvm in startup - if needed this should go to .zshrc.local

--- zsh/.zsh/configs/post/path.zsh
+++ zsh/.zsh/configs/post/path.zsh
@@ -1,13 +1,4 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/local/bin:$PATH"
 
-# load rbenv if available
-if command -v rbenv >/dev/null; then
-  eval "$(rbenv init - --no-rehash)"
-fi
-
-# nvm
-NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-
 export -U PATH
