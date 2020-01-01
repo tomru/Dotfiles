ee40fa4468e03eb1030c32bf7c51b93a60e3c195
Thomas Ruoff <ThomasRuoff@gmail.com>
2020-01-01 22:51:53 +0100

    set font and color scheme

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -2,6 +2,9 @@
 
 ;; Place your private configuration here
 
+(setq doom-font (font-spec :family "Fira Code" :size 18))
+(color-theme-sanityinc-tomorrow-eighties)
+
 (setq user-full-name "Thomas Ruoff")
 (setq user-mail-address "thomasruoff@gmail.com")
 
@@ -21,4 +24,3 @@
   (org-clock-persistence-insinuate))
 
 
-(setq doom-font (font-spec :family "Fira Code" :size 18))
