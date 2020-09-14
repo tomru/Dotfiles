85e84013cc682e3437ae7ad012898ce24eb4b0be
Thomas Ruoff <thomas.ruoff@daimler.com>
2020-09-14 08:30:51 +0200

    don't think I need this further

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -6,6 +6,4 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-(package! add-node-modules-path)
-(package! exec-path-from-shell)
 (package! color-theme-sanityinc-tomorrow)
